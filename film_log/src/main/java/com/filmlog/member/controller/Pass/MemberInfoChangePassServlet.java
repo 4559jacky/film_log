30ece936cf6505ae3948b4e12d2e543dba9b6c9a
4559jacky <4559jacky@gmail.com>
2025-03-05 18:04:50 +0900

    [jji] 로그인 기능 비동기로 다시 구현 및 개인정보수정 get 방식 -> post 방식 변경 commit

--- film_log/src/main/java/com/filmlog/member/controller/Pass/MemberInfoChangePassServlet.java
+++ film_log/src/main/java/com/filmlog/member/controller/Pass/MemberInfoChangePassServlet.java
@@ -9,8 +9,10 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import com.filmlog.member.model.service.MemberService;
+import com.filmlog.member.model.vo.Member;
 import com.filmlog.member.model.vo.MemberAddress;
 import com.filmlog.member.model.vo.MemberImg;
 
@@ -24,7 +26,15 @@ public class MemberInfoChangePassServlet extends HttpServlet {
     }
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		int memberNo = Integer.parseInt(request.getParameter("memberNo"));
+		
+		HttpSession session = request.getSession(false);
+		Member member = new Member();
+		
+		if(session != null && session.getAttribute("member") != null) {
+			member = (Member)session.getAttribute("member");
+		}
+		
+		int memberNo = member.getMemberNo();
 		
 		List<Integer> genres = memberService.selectMemberInterestGenreAll(memberNo);
 		MemberImg memberImg = memberService.selectMemberImg(memberNo);
